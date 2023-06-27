eed04654490fc70d6ae75f00e1f302938bed0889
Yuki Tanaka <uki00a@gmail.com>
2023-06-28 02:21:46 +0900

    chore: add `tools` directory (#21)

--- tools/build.ts
+++ tools/build.ts
@@ -3,11 +3,12 @@ const { stdout: gitOutput } = await new Deno.Command("git", {
 }).output();
 const revision = new TextDecoder().decode(gitOutput).trim();
 Deno.env.set("DENO_DEPLOYMENT_ID", revision);
+Deno.env.set("PORT", "8000");
 
-import("./main.ts");
+import(import.meta.resolve("../main.ts"));
 
 await new Promise((ok) => {
-  setTimeout(ok, 1000);
+  setTimeout(ok, 10000);
 });
 
 const res = await fetch("http://localhost:8000");
@@ -17,7 +18,7 @@ if (!res.ok) {
 }
 
 const html = await res.text();
-const buildDir = new URL("./build", import.meta.url).pathname;
+const buildDir = new URL("../build", import.meta.url).pathname;
 await Deno.mkdir(buildDir, { recursive: true });
 await Deno.writeTextFile(`${buildDir}/index.html`, html);
 
