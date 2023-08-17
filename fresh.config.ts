b02ed75c279b6e816ad0ab693e3f80a8c97a10ea
uki00a <uki00a@gmail.com>
2023-08-17 22:45:03 +0900

    chore: fmt

--- fresh.config.ts
+++ fresh.config.ts
@@ -4,5 +4,5 @@ import twindv1 from "$fresh/plugins/twindv1.ts";
 import twindConfig from "./twind.config.ts";
 
 export default defineConfig({
-  plugins: [twindv1(twindConfig)]
+  plugins: [twindv1(twindConfig)],
 });
