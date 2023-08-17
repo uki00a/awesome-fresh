5dd6f7b59712887896680b472d12e38df3615c53
uki00a <uki00a@gmail.com>
2023-08-17 22:43:57 +0900

    chore: migrate to `fresh.config.ts`

---
+++ fresh.config.ts
@@ -0,0 +1,8 @@
+import { defineConfig } from "$fresh/server.ts";
+
+import twindv1 from "$fresh/plugins/twindv1.ts";
+import twindConfig from "./twind.config.ts";
+
+export default defineConfig({
+  plugins: [twindv1(twindConfig)]
+});
