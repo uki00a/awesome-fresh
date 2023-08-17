9aa0bddbd9a82b4843e39fb4284ff773afd89d7f
uki00a <uki00a@gmail.com>
2023-08-17 21:50:11 +0900

    fix: title

--- routes/_app.tsx
+++ routes/_app.tsx
@@ -6,7 +6,7 @@ export default function App({ Component }: AppProps) {
       <head>
         <meta charSet="utf-8" />
         <meta name="viewport" content="width=device-width, initial-scale=1.0" />
-        <title>awesome-fresh</title>
+        <title>Awesome Fresh</title>
       </head>
       <body>
         <Component />
