6e677084d96d2d2404386a06510e0d601bd56074
Yuki Tanaka <uki00a@gmail.com>
2024-12-30 19:30:16 +0900

    fix: could not get `styles.css` on GitHub Pages (#43)
    Follows up to #42

--- routes/_app.tsx
+++ routes/_app.tsx
@@ -1,14 +1,17 @@
 import { asset } from "fresh/runtime";
 import type { PageProps } from "fresh";
 
-export default function App({ Component }: PageProps) {
+export default function App({ config, Component }: PageProps) {
   return (
     <html>
       <head>
         <meta charSet="utf-8" />
         <meta name="viewport" content="width=device-width, initial-scale=1.0" />
         <title>Awesome Fresh</title>
-        <link rel="stylesheet" href={asset("/styles.css")} />
+        <link
+          rel="stylesheet"
+          href={`${config.basePath}${asset("/styles.css")}`}
+        />
       </head>
       <body>
         <Component />
