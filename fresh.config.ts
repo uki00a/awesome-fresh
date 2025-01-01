d8113e03447632a8787698c777b8a58b3dc5296e
Yuki Tanaka <uki00a@gmail.com>
2025-01-01 17:36:21 +0900

    chore: remove files related to fresh v1 (#45)
    Forgot to remove these files in #40.

--- fresh.config.ts
+++
@@ -1,8 +0,0 @@
-import { defineConfig } from "$fresh/server.ts";
-
-import twindv1 from "$fresh/plugins/twindv1.ts";
-import twindConfig from "./twind.config.ts";
-
-export default defineConfig({
-  plugins: [twindv1(twindConfig)],
-});
