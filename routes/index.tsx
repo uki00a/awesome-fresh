d973f91caa18551688f3698518f5e490176ce4a3
Yuki Tanaka <uki00a@gmail.com>
2025-02-22 17:15:41 +0900

    deps: bump Deno to `2.2.1` (#47)

--- routes/index.tsx
+++ routes/index.tsx
@@ -20,6 +20,7 @@ export default define.page(async () => {
         data-color-mode="auto"
         data-dark-theme="dark"
         class="p-4 mx-auto max-w-screen-md markdown-body"
+        // deno-lint-ignore react-no-danger -- `content` is not externally given
         dangerouslySetInnerHTML={{ __html: content }}
       />
     </>
